afcfac2184da855ec96b81a7e48bbffcc1c1ac96
Roshni1809 <roshni18shukla@gmail.com>
2021-12-04 15:26:59 +0530

    iframe added in contact page

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/style-prop-object */
+/* eslint-disable jsx-a11y/iframe-has-title */
 import React from "react";
 import { makeStyles, Typography } from "@material-ui/core";
 import FooterContact from "../Common/FooterContact";
@@ -9,6 +11,9 @@ const useStyles = makeStyles((theme) => ({
   },
   heading:{
     fontFamily:" -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen','Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',sans-seri !important"
+  },
+  map:{
+    textAlign:"center"
   }
 }));
 
@@ -18,6 +23,15 @@ function Contact() {
     <div className={classes.container}>
 
       <Typography variant="h2" align="center"className={classes.heading}>Contact Us</Typography>
+     <div className={classes.map}>
+      <iframe id="iframeid"
+        width="80%"
+        height="500"
+        src="https://www.google.com/maps/embed?api=1&origin=Space+Needle+Seattle+WA&destination=Pike+Place+Market+Seattle+WA&travelmode=bicycling">
+
+        </iframe>
+        </div>
+      
       <Typography variant="body2" align="center">abchjjkfjjgjgj</Typography>
       <Typography variant="body2" align="center">abcd</Typography>
 
