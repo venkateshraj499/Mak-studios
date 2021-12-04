6140a5297d314d1bc1c50a8774f7a1afef16475f
Venkatesh <venkateshraj499@gmail.com>
2021-12-04 15:30:38 +0530

    Pretty

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -5,38 +5,46 @@ import { makeStyles, Typography } from "@material-ui/core";
 import FooterContact from "../Common/FooterContact";
 
 const useStyles = makeStyles((theme) => ({
-
-  container:{
-    justifyContent:"center",
+  container: {
+    justifyContent: "center",
   },
-  heading:{
-    fontFamily:" -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen','Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',sans-seri !important"
+  heading: {
+    fontFamily:
+      " -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen','Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',sans-seri !important",
   },
-  map:{
-    textAlign:"center"
-  }
+  map: {
+    textAlign: "center",
+  },
+  iframe: {
+    border: "none",
+  },
 }));
 
 function Contact() {
   const classes = useStyles();
   return (
     <div className={classes.container}>
+      <Typography variant="h4" align="center" className={classes.heading}>
+        CONTACT US
+      </Typography>
+      <div className={classes.map}>
+        <iframe
+          id="iframeid"
+          width="80%"
+          height="500"
+          className={classes.iframe}
+          src="https://www.google.com/maps/embed?api=1&origin=Space+Needle+Seattle+WA&destination=Pike+Place+Market+Seattle+WA&travelmode=bicycling"
+        ></iframe>
+      </div>
 
-      <Typography variant="h2" align="center"className={classes.heading}>Contact Us</Typography>
-     <div className={classes.map}>
-      <iframe id="iframeid"
-        width="80%"
-        height="500"
-        src="https://www.google.com/maps/embed?api=1&origin=Space+Needle+Seattle+WA&destination=Pike+Place+Market+Seattle+WA&travelmode=bicycling">
-
-        </iframe>
-        </div>
-      
-      <Typography variant="body2" align="center">abchjjkfjjgjgj</Typography>
-      <Typography variant="body2" align="center">abcd</Typography>
+      <Typography variant="body2" align="center">
+        abchjjkfjjgjgj
+      </Typography>
+      <Typography variant="body2" align="center">
+        abcd
+      </Typography>
 
       <FooterContact />
-
     </div>
   );
 }
